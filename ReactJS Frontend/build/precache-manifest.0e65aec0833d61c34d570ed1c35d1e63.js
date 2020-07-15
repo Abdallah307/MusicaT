42b66116499a5af8a8753bc9711bf6fce3f9d1f7
Abdallah307 <51496376+Abdallah307@users.noreply.github.com>
2020-07-15 21:33:35 +0300

    Add files via upload

---
+++ ReactJS Frontend/build/precache-manifest.0e65aec0833d61c34d570ed1c35d1e63.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37ae284e94a438d73433654d48b59719",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c69e0a0c7eb5a63f423c",
+    "url": "/static/css/main.1fe20b84.chunk.css"
+  },
+  {
+    "revision": "d2e36d4a84ab16c41ba8",
+    "url": "/static/js/2.1ab5078c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1ab5078c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c69e0a0c7eb5a63f423c",
+    "url": "/static/js/main.a7b6a85e.chunk.js"
+  },
+  {
+    "revision": "77c4b1401450819c2fc7",
+    "url": "/static/js/runtime-main.184d35f6.js"
+  },
+  {
+    "revision": "f14fb5e3bb3dc2b9db041d2f25f57341",
+    "url": "/static/media/Singer.f14fb5e3.jpg"
+  },
+  {
+    "revision": "a991c21378ca83fa59cb83a3e895e753",
+    "url": "/static/media/abdallah.a991c213.jpg"
+  },
+  {
+    "revision": "9172e17c83aaa13028cdefc7d86f3b1f",
+    "url": "/static/media/background1.9172e17c.svg"
+  },
+  {
+    "revision": "815c02468e9bbcb05556b25ab0fc0d1c",
+    "url": "/static/media/undraw_media_player_ylg8.815c0246.svg"
+  }
+]);
